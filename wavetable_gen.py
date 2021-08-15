a80db66f296cf46b9bc87183fb274e20cffc6f05
Ryszard Jezierski <jezierski.ryszard@gmail.com>
2021-08-16 01:31:27 +0200

    wavetable_gen.py now generates wavetable based on spectrum, with built-in profiles for saw, square and triangle waves

--- wavetable_gen.py
+++ wavetable_gen.py
@@ -1,14 +1,36 @@
 #!/usr/bin/env python
 import math as m
-import matplotlib.pyplot as plt
 import sys
 
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy.fft import irfft
 
-def gen_saw(t, n_harmonics, amp=1.0):
-    sample = 0
-    for i in range(1, n_harmonics + 1):
-        sample += (-1) ** i * m.sin(2 * m.pi * t * i) / i
-    return amp * (- 1 / m.pi * sample)
+
+def gen_saw_spectrum(length, amp=1.0):
+    tab = [(-1) ** i / i for i in range(1, length - 1)]
+    tab = np.concatenate(([0], tab))
+    # multiply by j to change from sin to cos
+    return amp * tab.astype(complex) * (0 + 1j)
+
+
+def gen_square_spectrum(length, amp=1.0):
+    tab = np.array(length * [0.0])
+    for i in range(1, length, 2):
+        tab[i] = 1 / i
+    return amp * tab.astype(complex) * (0 - 1j)
+
+
+def gen_triangle_spectrum(length, amp=1.0):  # TODO: this doesn't work yet
+    tab = np.array(length * [0.0])
+    for i in range(1, length // 2):
+        n = 2 * i + 1
+        tab[2 * i - 1] = ((-1) ** i) / (n ** 2)
+    return amp * tab.astype(complex) * (0 + 1j)
+
+
+def normalize(table, max_val=1.0):
+    return table * (max_val / table.max())
 
 
 def write_wavetables(tables, name, f_lowest, f_highest):
@@ -24,8 +46,17 @@ def write_wavetables(tables, name, f_lowest, f_highest):
 
 
 def main():
-    f_low = int(sys.argv[1])
-    f_high = int(sys.argv[2])
+    name = sys.argv[1]
+    if name == 'saw':
+        spectrum = gen_saw_spectrum(2048)
+    elif name == 'square':
+        spectrum = gen_square_spectrum(2048)
+    elif name == 'triangle':
+        spectrum = gen_triangle_spectrum(2048)
+    else:
+        spectrum = np.genfromtxt(name, dtype=complex)
+    f_low = int(sys.argv[2])
+    f_high = int(sys.argv[3])
     amp = 0.8
     octaves = m.ceil(m.log2(f_high / f_low))
     fig, axs = plt.subplots(m.ceil(octaves / 2), 2, sharex='all', sharey='all')
@@ -33,15 +64,17 @@ def main():
     table = octaves * [None]
     for octave in range(octaves):
         n = m.floor(14000 / (f_low * 2 ** octave))
+        s = spectrum
+        s[n + 1:] = 0
         print("harmonics for " + str(octave) + " octave: " + str(n))
-        table[octave] = [gen_saw(i / 2048, n, amp) for i in range(2048)]
+        table[octave] = normalize(irfft(s, 2048) * amp)
 
         axs[octave // 2, octave % 2].set_title('octave ' + str(octave))
         axs[octave // 2, octave % 2].plot(table[octave])
 
     plt.show()
 
-    write_wavetables(table, 'saw.synthwave', f_low, f_high)
+    write_wavetables(table, name + '.synthwave', f_low, f_high)
 
 
 if __name__ == '__main__':
