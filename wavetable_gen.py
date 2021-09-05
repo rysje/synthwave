e49543b0f708746bc8b2cbfee253a14240feab7b
Ryszard Jezierski <jezierski.ryszard@gmail.com>
2021-09-05 17:07:50 +0200

    wavetable_gen.py refactor and documentation

--- wavetable_gen.py
+++ wavetable_gen.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+"""Tools for generating wavetables
+"""
 import math as m
 import sys
 
@@ -7,21 +9,39 @@ import numpy as np
 from numpy.fft import irfft
 
 
-def gen_saw_spectrum(length, amp=1.0):
-    tab = [(-1) ** i / i for i in range(1, length - 1)]
+def gen_saw_spectrum(length: int, amp=1.0) -> np.ndarray:
+    """Generate spectrum of real sawtooth wave signal.
+
+    :param length: length of the resulting spectrum; half of window size
+    :param amp: amplitude of the spectrum
+    :return: a complex array containing spectrum values
+    """
+    tab = np.array([(-1) ** i / i for i in range(1, length - 1)])
     tab = np.concatenate(([0], tab))
     # multiply by j to change from sin to cos
     return amp * tab.astype(complex) * (0 + 1j)
 
 
-def gen_square_spectrum(length, amp=1.0):
+def gen_square_spectrum(length: int, amp=1.0) -> np.ndarray:
+    """Generate spectrum of real square wave signal.
+
+    :param length: length of the resulting spectrum; half of window size
+    :param amp: amplitude of the spectrum
+    :return: a complex array containing spectrum values
+    """
     tab = np.array(length * [0.0])
     for i in range(1, length, 2):
         tab[i] = 1 / i
     return amp * tab.astype(complex) * (0 - 1j)
 
 
-def gen_triangle_spectrum(length, amp=1.0):  # TODO: this doesn't work yet
+def gen_triangle_spectrum(length: int, amp=1.0) -> np.ndarray:  # TODO: this doesn't work yet
+    """Generate spectrum of real triangle wave signal.
+
+    :param length: length of the resulting spectrum; half of window size
+    :param amp: amplitude of the spectrum
+    :return: a complex array containing spectrum values
+    """
     tab = np.array(length * [0.0])
     for i in range(1, length // 2):
         n = 2 * i + 1
@@ -29,23 +49,41 @@ def gen_triangle_spectrum(length, amp=1.0):  # TODO: this doesn't work yet
     return amp * tab.astype(complex) * (0 + 1j)
 
 
-def normalize(table, max_val=1.0):
-    return table * (max_val / table.max())
+def normalize(table: np.ndarray, max_val=1.0) -> np.ndarray:
+    """Normalize contents input array to given max value.
+
+    :param table: input array
+    :param max_val: maximum array value
+    :return: normalized array
+    """
+    return table * (max_val / np.abs(table).max(0))
 
 
-def write_wavetables(tables, name, f_lowest, f_highest):
-    with open(name, 'w') as f:
+def write_wavetables(tables: list, name: str, f_lowest: float, f_highest: float) -> None:
+    """Write wavetables to the output file.
+
+    :param tables: list of ndarrays containing wavetables
+    :param name: output file name
+    :param f_lowest: lowest frequency represented by the wavetables
+    :param f_highest: highest frequency represented by the wavetables
+    """
+    with open(name, 'w', encoding='utf-8') as file:
         for i, table in enumerate(tables):
             f_low = f_lowest * 2 ** i
             f_high = f_low * 2 if (f_low * 2 < f_highest) else f_highest
-            f.write(str(f_low) + '\n')
-            f.write(str(f_high) + '\n')
-            f.write(str(len(table)) + '\n')
-            f.writelines('\n'.join([str(i) for i in table]))
-            f.write('\n')
+            file.write(str(f_low) + '\n')
+            file.write(str(f_high) + '\n')
+            file.write(str(len(table)) + '\n')
+            file.writelines('\n'.join([str(i) for i in table]))
+            file.write('\n')
 
 
 def main():
+    """Generate wavetables
+
+    This function can generate wavetables either based on name
+    (saw, square, triangle), or given file containing spectrum values
+    """
     name = sys.argv[1]
     if name == 'saw':
         spectrum = gen_saw_spectrum(2048)
@@ -64,10 +102,10 @@ def main():
     table = octaves * [None]
     for octave in range(octaves):
         n = m.floor(14000 / (f_low * 2 ** octave))
-        s = spectrum
-        s[n + 1:] = 0
-        print("harmonics for " + str(octave) + " octave: " + str(n))
-        table[octave] = normalize(irfft(s, 2048) * amp)
+        spectrum_fragment = spectrum
+        spectrum_fragment[n + 1:] = 0
+        print('harmonics for ' + str(octave) + ' octave: ' + str(n))
+        table[octave] = normalize(irfft(spectrum_fragment, 2048) * amp)
 
         axs[octave // 2, octave % 2].set_title('octave ' + str(octave))
         axs[octave // 2, octave % 2].plot(table[octave])
