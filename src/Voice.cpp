5aa2fcb6988abd4cf93e859961a0e9a98a38229d
Ryszard Jezierski <jezierski.ryszard@gmail.com>
2021-09-05 18:09:51 +0200

    POC amplitude ADSR

--- src/Voice.cpp
+++ src/Voice.cpp
@@ -3,7 +3,7 @@
 
 Voice::Voice(double frequency, jack_nframes_t sample_rate, Wavetable &wavetable)
 	: phase(0), baseFrequency(frequency), wavetable(wavetable), noteOn(false),
-	sampleRate(sample_rate), state(VoiceState::Inactive), amplitude(1.0), buffersInCurrentState(0)
+	sampleRate(sample_rate), state(VoiceState::Inactive), amplitude(1.0), buffersInCurrentState(0), ampAdsr{0.0, attackLen, 1.0, decayLen, sustainLev, releaseLen, 0.0}
 {
 	ramp_step = frequency / sampleRate;
 	std::cout << frequency << "\t" <<ramp_step << std::endl;
@@ -11,13 +11,35 @@ Voice::Voice(double frequency, jack_nframes_t sample_rate, Wavetable &wavetable)
 
 void Voice::Process(jack_default_audio_sample_t* buffer, jack_nframes_t nframes)
 {
-	if (state == VoiceState::Sustain) {
+	if (state != VoiceState::Inactive) {
+		double envVal = ampAdsr.value(state, buffersInCurrentState);
 		double frequency = baseFrequency * freqMod;
 		for (int i = 0; i < nframes; i++) {
 			ramp_step = frequency / sampleRate;
-			buffer[i] += amplitude * 0.2f * wavetable.returnSample(frequency, phase);
+			buffer[i] += envVal * amplitude * 0.9f * wavetable.returnSample(frequency, phase);
 			phase += ramp_step;
 			phase = (phase > 1.0) ? phase - 1.0 : phase;
+		}
+		buffersInCurrentState++;
+		switch (state) {
+			case VoiceState::Attack:
+				if (buffersInCurrentState >= attackLen) {
+					state = VoiceState::Decay;
+					buffersInCurrentState = 0;
+					break;
+				}
+			case VoiceState::Decay:
+				if (buffersInCurrentState >= decayLen) {
+					state = VoiceState::Sustain;
+					buffersInCurrentState = 0;
+					break;
+				}
+			case VoiceState::Release:
+				if (buffersInCurrentState >= releaseLen) {
+					state = VoiceState::Inactive;
+					buffersInCurrentState = 0;
+					break;
+				}
 		}
 	}
 }
@@ -25,7 +47,7 @@ void Voice::Process(jack_default_audio_sample_t* buffer, jack_nframes_t nframes)
 bool Voice::isActiveInCurrentBuffer()
 {
 	bool active = false;
-	if (!sustainPedalOn && !noteOn) {
+	if (!sustainPedalOn && !noteOn && (state != VoiceState::Release)) {
 		state = VoiceState::Inactive;
 	}
 	if (state != VoiceState::Inactive) {
@@ -36,9 +58,10 @@ bool Voice::isActiveInCurrentBuffer()
 
 void Voice::on(unsigned char velocity)
 {
+	buffersInCurrentState = 0;
 	std::cout << (int) velocity << '\n';
 	noteOn = true;
-	state = VoiceState::Sustain;
+	state = VoiceState::Attack;
 	amplitude = (double) velocity / 127.0;
 }
 
@@ -46,7 +69,7 @@ void Voice::off()
 {
 	noteOn = false;
 	if (!sustainPedalOn) {
-		state = VoiceState::Inactive;
+		state = VoiceState::Release;
 	}
 }
 
