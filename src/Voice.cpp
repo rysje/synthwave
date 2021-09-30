d2d534609aeca60b8382508466e3b69b65475a1e
Ryszard Jezierski <jezierski.ryszard@gmail.com>
2021-09-30 21:52:12 +0200

    sustain works but slow

--- src/Voice.cpp
+++ src/Voice.cpp
@@ -20,48 +20,37 @@ Voice::Voice(double frequency, jack_nframes_t sample_rate, Wavetable &wavetable)
 
 void Voice::Process(jack_default_audio_sample_t* buffer, jack_nframes_t nframes)
 {
-	if (state == VoiceState::Sustain) {
-		double frequency = baseFrequency * freqMod;
-		ramp_step = frequency / sampleRate;
-		for (int i = 0; i < nframes; i++) {
-//			buffer[i] += 0.2f * wavetable.returnSample(frequency, phase);
-//			buffer[i] *= ampAdsr.tick();
-//			buffer[i] = biquad.tick(buffer[i]);
-			jack_default_audio_sample_t sample = 0.2f * wavetable.returnSample(frequency, phase);
-			sample *= ampAdsr.tick();
-			sample = biquad.tick(sample);
-			buffer[i] += sample;
-			phase += ramp_step;
-			phase = (phase > 1.0) ? phase - 1.0 : phase;
-		}
+	double frequency = baseFrequency * freqMod;
+	ramp_step = frequency / sampleRate;
+	for (int i = 0; i < nframes; i++) {
+		jack_default_audio_sample_t sample = 0.2f * wavetable.returnSample(frequency, phase);
+		sample *= ampAdsr.tick();
+		sample = biquad.tick(sample);
+		buffer[i] += sample;
+		phase += ramp_step;
+		phase = (phase > 1.0) ? phase - 1.0 : phase;
 	}
 }
 
 bool Voice::isActiveInCurrentBuffer()
 {
-	bool active = false;
-	if (!sustainPedalOn && !noteOn) {
-		state = VoiceState::Inactive;
+	if (!noteOn && !sustainPedalOn) {
+		ampAdsr.keyOff();
 	}
-	if (state != VoiceState::Inactive) {
-		active = true;
-	}
-	return active;
+	return noteOn || (ampAdsr.getState() != stk::ADSR::IDLE);
 }
 
 void Voice::on()
 {
 	noteOn = true;
 	ampAdsr.keyOn();
-	state = VoiceState::Sustain;
 }
 
 void Voice::off()
 {
-	ampAdsr.keyOff();
 	noteOn = false;
 	if (!sustainPedalOn) {
-		state = VoiceState::Inactive;
+		ampAdsr.keyOff();
 	}
 }
 
